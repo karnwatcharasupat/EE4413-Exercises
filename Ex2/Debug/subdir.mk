1a2ef61dc8bd8fb1a8aad19bb808c1a52b445bd3
Karn Watcharasupat <karn001@e.ntu.edu.sg>
2020-10-16 14:52:57 +0800

    Exercise 2

---
+++ Ex2/Debug/subdir.mk
@@ -0,0 +1,49 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../main.c \
+../sar.c 
+
+CMD_SRCS += \
+../lnkx.cmd 
+
+OBJS += \
+./main.obj \
+./sar.obj 
+
+C_DEPS += \
+./main.pp \
+./sar.pp 
+
+OBJS__QTD += \
+".\main.obj" \
+".\sar.obj" 
+
+C_DEPS__QTD += \
+".\main.pp" \
+".\sar.pp" 
+
+C_SRCS_QUOTED += \
+"../main.c" \
+"../sar.c" 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+main.obj: ../main.c $(GEN_OPTS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: Compiler'
+	"C:/Program Files (x86)/Texas Instruments/ccsv4/tools/compiler/c5500/bin/cl55" -v5515 -g --include_path="C:/Program Files (x86)/Texas Instruments/ccsv4/tools/compiler/c5500/include" --include_path="C:/Users/karn001/Desktop/Exercises/Part 2 of 2_By Prof Seow CK/EE4413_C515/C5515_Support_Files/C5515_Includes" --diag_warning=225 --ptrdiff_size=32 --memory_model=large --preproc_with_compile --preproc_dependency="main.pp" $(GEN_OPTS_QUOTED) $(subst #,$(wildcard $(subst $(SPACE),\$(SPACE),$<)),"#")
+	@echo 'Finished building: $<'
+	@echo ' '
+
+sar.obj: ../sar.c $(GEN_OPTS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: Compiler'
+	"C:/Program Files (x86)/Texas Instruments/ccsv4/tools/compiler/c5500/bin/cl55" -v5515 -g --include_path="C:/Program Files (x86)/Texas Instruments/ccsv4/tools/compiler/c5500/include" --include_path="C:/Users/karn001/Desktop/Exercises/Part 2 of 2_By Prof Seow CK/EE4413_C515/C5515_Support_Files/C5515_Includes" --diag_warning=225 --ptrdiff_size=32 --memory_model=large --preproc_with_compile --preproc_dependency="sar.pp" $(GEN_OPTS_QUOTED) $(subst #,$(wildcard $(subst $(SPACE),\$(SPACE),$<)),"#")
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
